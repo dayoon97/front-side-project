318778f7e8290c8d4f029d014e183c7ab1ff1d7e
심다윤 <ekdbsekdbs@gmail.com>
2022-01-10 22:59:59 +0900

    front-side-project intro.js
    이미지 변경 및 body 스타일 추가

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,11 +1,11 @@
 import React from "react";
-import backgroundImage from "../img/football.jpg";
+import backgroundImage from "../img/woman-football.jpg";
 import "../style/intro.css";
 
 function Intro() {
     return (
         <div className="Intro">
-            <img src={backgroundImage}/>
+            <img src={backgroundImage} className="back-img"/>
         </div>
     );
 }
